a12ce0d0c2c105c3e95ace4ea86cc61068670c08
Alex Mehr <alirezaalipouri@gmail.com>
2018-07-01 10:19:54 -0700

    in progress

---
+++ liri.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+// var key = require("./key");
+var fs = require("fs");
+
+var twitter = new twitter(keys.twitter);
+var spotify = new spotify(keys.spotify);
+
+
+module.exports = "key.js";
